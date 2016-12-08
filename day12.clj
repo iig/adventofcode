ec76c4d6c5353fc5931eb0f9ae33dad484afdbb8
Igor I. Ges <igor@gesii.net>
2016-12-08 10:26:21 -0600

    Cleaned everything up (sort of) and added 2016

--- day12.clj
+++
@@ -1,9 +0,0 @@
-(ns adventofcode.day12)
-
-;;again, why write a text parser when you have regexes?
-(->> input
-     (re-seq #"-?\d+")
-     (map read-string)
-     (reduce +))
-
-;; part 2: to be continued
